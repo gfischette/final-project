8ae54311b8c107436973129ba821e099f8e5718f
gfischette <gfischette@ufl.edu>
2023-12-09 23:56:54 -0500

    Code credit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-// This code was taken from https://github.com/kayleyseow/Personality-Quiz and modifed to work with my app.
+// this code was taken from https://github.com/kayleyseow/Personality-Quiz and modifed to work with my app.
 
 var questionCount = 0;
 var result1Score = 0; // fiction
